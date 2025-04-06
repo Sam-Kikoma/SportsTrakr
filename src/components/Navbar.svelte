f225ca6c68c9e99ef576cabd3f7845f367888dc5
Sam Kikoma <kikomasamson@gmail.com>
2025-04-06 14:47:30 +0200

    refactor: updated to conditionally display links depending on props

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,38 +1,62 @@
 <script lang="ts">
 	import Icon from '@iconify/svelte';
+
+	// Declare the props for the Navbar component and make them optional
+	export let signupLink: string | undefined = undefined; // Default value is undefined, so link is not rendered
+	export let loginLink: string | undefined = undefined; // Default value is undefined, so link is not rendered
+	export let logoutLink: string | undefined = undefined; // Default value is undefined, so link is not rendered
+
 	let showMenu = true;
 	const toggleMenu = () => {
 		showMenu = !showMenu;
 	};
 </script>
 
-<nav class="flex max-w-full items-center justify-between px-8 py-8 text-white">
-	<div><a href="/" class="text-2xl font-bold text-amber-600">Trakr</a></div>
-	<div class="hidden md:flex md:gap-8">
-		<a href="/">Scores</a>
-		<a href="/">Favourites</a>
-		<a href="/">News</a>
+<nav class="flex max-w-full items-center justify-between text-center">
+	<div>
+		<a href="/" class="text-2xl font-bold text-amber-600">Trakr</a>
 	</div>
-	<div class="hidden md:flex md:gap-4">
-		<a href="/signup" class="hover:underline">Sign Up</a>
-		<a href="/login">Login</a>
-	</div>
+
+	<!-- Desktop menu -->
+	{#if signupLink || loginLink || logoutLink}
+		<div class="hidden md:flex md:gap-8">
+			{#if signupLink}
+				<a href={signupLink} class="hover:underline">Sign Up</a>
+			{/if}
+			{#if loginLink}
+				<a href={loginLink}>Login</a>
+			{/if}
+			{#if logoutLink}
+				<a href={logoutLink}>Logout</a>
+			{/if}
+		</div>
+	{/if}
+
 	<!-- Hamburger -->
 	{#if !showMenu}
-		<button class="md:hidden" on:click={toggleMenu}
-			><Icon icon="radix-icons:hamburger-menu" width="15" height="15" /></button
-		>
+		<button class="md:hidden" on:click={toggleMenu}>
+			<Icon icon="radix-icons:hamburger-menu" width="15" height="15" />
+		</button>
 	{:else}
 		<button class="md:hidden" on:click={toggleMenu}>
 			<Icon icon="radix-icons:cross-2" width="15" height="15" />
 		</button>
 	{/if}
+
 	<!-- Mobile menu -->
-	<div
-		class={`absolute top-[20%] left-0 flex h-screen w-full flex-col gap-8 text-center md:hidden ${showMenu ? '' : 'hidden'}`}
-	>
-		<a href="/">Scores</a>
-		<a href="/">Favourites</a>
-		<a href="/">News</a>
-	</div>
+	{#if signupLink || loginLink || logoutLink}
+		<div
+			class={`absolute top-[20%] left-0 flex h-screen w-full flex-col gap-8 text-center md:hidden ${showMenu ? '' : 'hidden'}`}
+		>
+			{#if signupLink}
+				<a href={signupLink} class="hover:underline">Sign Up</a>
+			{/if}
+			{#if loginLink}
+				<a href={loginLink}>Login</a>
+			{/if}
+			{#if logoutLink}
+				<a href={logoutLink}>Logout</a>
+			{/if}
+		</div>
+	{/if}
 </nav>
