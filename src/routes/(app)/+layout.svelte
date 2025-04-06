1e894ae48113e3be251f2d3c7c6a07ef3aff0a02
Sam Kikoma <kikomasamson@gmail.com>
2025-04-06 14:51:48 +0200

    refactor: deleted

--- src/routes/(app)/+layout.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	import Navbar from '../../components/Navbar.svelte';
-	let { children } = $props();
-</script>
-
-<div class="h-screen max-w-full bg-gray-950">
-	<Navbar />
-	{@render children()}
-</div>
