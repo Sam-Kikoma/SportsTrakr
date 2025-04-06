9f1ea64c1190706dd31a32b8d7b3ecc082fec725
Sam Kikoma <kikomasamson@gmail.com>
2025-04-06 14:45:58 +0200

    feat: initial dashboard

---
+++ src/routes/(app)/dashboard/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import Navbar from '../../../components/Navbar.svelte';
+
+	export let data;
+</script>
+
+<div class="flex h-screen flex-col justify-between bg-gray-900 text-white">
+	<Navbar logoutLink="/logout" />
+</div>
+
+<style>
+</style>
