8c9633394d6cf626df2471cf668b1a19e1edb686
Sam Kikoma <kikomasamson@gmail.com>
2025-04-27 14:07:17 +0200

    feat: Tab component to cycle through categories

---
+++ src/components/Tabs.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	interface Props {
+		items: string[];
+		active: string;
+		onTabChange: (item: string) => void;
+	}
+	let { items, active, onTabChange }: Props = $props();
+</script>
+
+<div>
+	<div class="flex justify-center gap-16">
+		{#each items as item}
+			<button
+				class={item === active ? 'text-amber-400 underline underline-offset-8' : ''}
+				onclick={() => onTabChange(item)}
+			>
+				{item}
+			</button>
+		{/each}
+	</div>
+</div>
+
+<style>
+</style>
