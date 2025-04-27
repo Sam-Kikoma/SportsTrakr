c9c6da5bc4b266248fff41630ec41d6f407157b6
Sam Kikoma <kikomasamson@gmail.com>
2025-04-27 14:21:12 +0200

    feat: dashboard with tab component

--- src/routes/(app)/dashboard/+page.svelte
+++ src/routes/(app)/dashboard/+page.svelte
@@ -1,11 +1,35 @@
 <script lang="ts">
+	import Basketball from '../../../components/Basketball.svelte';
+	import Football from '../../../components/Football.svelte';
 	import Navbar from '../../../components/Navbar.svelte';
+	import Tabs from '../../../components/Tabs.svelte';
+	import Nfl from '../../../components/Nfl.svelte';
+	import Favourites from '../../../components/Favourites.svelte';
 
-	export let data;
+	// Tabs
+	let items = ['football', 'basketball', 'nfl', 'favourites'];
+	let activeItem = $state('football');
+	const handleChange = (item: string) => {
+		activeItem = item;
+	};
 </script>
 
-<div class="flex h-screen flex-col justify-between bg-gray-900 text-white">
-	<Navbar logoutLink="/logout" />
+<div class="flex h-screen flex-col bg-gray-900 px-6 py-4 text-white">
+	<div>
+		<Navbar logoutLink="/logout" />
+	</div>
+	<Tabs {items} active={activeItem} onTabChange={handleChange} />
+	<div>
+		{#if activeItem === 'football'}
+			<Football />
+		{:else if activeItem === 'basketball'}
+			<Basketball />
+		{:else if activeItem === 'nfl'}
+			<Nfl />
+		{:else if activeItem === 'favourites'}
+			<Favourites />
+		{/if}
+	</div>
 </div>
 
 <style>
